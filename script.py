bf2bcc53cc439a63736418cb5909ad3cd28379b2
Sirbu <dragos.daniel199@gmail.com>
2022-01-05 08:18:17 +0200

    updates

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ from mcstatus import MinecraftServer
 server = MinecraftServer.lookup("157.90.221.188:6055")
 status = server.status()
 print(f"The server has {status.players.online} players and replied in {status.latency} ms")
+
+# query = server.query()
+# print(f"The server has the following players online: {', '.join(query.players.names)}")
