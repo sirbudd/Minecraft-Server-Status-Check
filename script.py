008a3cd9b66a56a9c14a2faae2afd9f9d29f8c6d
Sirbu <dragos.daniel199@gmail.com>
2022-01-07 12:31:13 +0200

    fixed query implementation

--- script.py
+++ script.py
@@ -3,15 +3,30 @@ from multiprocessing import shared_memory
 import logging
 from functions import *
 from mcstatus import MinecraftServer
-
+import socket
 
 config = open_cfg()  
 logging.basicConfig(filename='logs.log', level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s') #logging file config
 
 
 def CheckServerStatus():
-    server = MinecraftServer.lookup(config['server_ip'])
-    
+    try:
+        server = MinecraftServer.lookup(config['server_ip'])
+        query = server.query()
+        status = server.status()
+        print(status.latency)
+    except socket.timeout:
+        print("Socket timed out")
+        print("Maybe check your server's IP?")
+
+
+    # print(f"Server version : {query.software.version}")
+    # print(f"Server MOTD : {status.description}")
+    # print(f"Players : {query.players.names}")
+    logging.info(f"Server version : {query.software.version}")
+    logging.info(f"Server MOTD : {status.description}")
+    logging.info(f"Players : {query.players.names}")
+    logging.info(f"==============")
 
 if __name__ == '__main__':
     CheckServerStatus()
