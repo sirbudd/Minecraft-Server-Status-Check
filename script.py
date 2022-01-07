ee1ac320c31e8cabd58228ee7760b3bf5ba8e0f1
Sirbu <dragos.daniel199@gmail.com>
2022-01-08 00:44:24 +0200

    bug fixing

--- script.py
+++ script.py
@@ -1,20 +1,24 @@
-import pickle
+#import pickle
 from multiprocessing import shared_memory
 import logging
 from functions import *
 from mcstatus import MinecraftServer
 import socket
 
-config = open_cfg()  
-logging.basicConfig(filename='logs.log', level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s') #logging file config
 
+logging.basicConfig(filename='logs.log', level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s') #logging file config
+config = open_cfg()
 
 def CheckServerStatus():
     try:
         server = MinecraftServer.lookup(config['server_ip'])
         query = server.query()
         status = server.status()
-        print(status.latency)
+        logging.info(status.latency)
+        logging.info(f"Server version : {query.software.version}")
+        logging.info(f"Server MOTD : {status.description}")
+        logging.info(f"Players : {query.players.names}")
+        logging.info(f"==============")
     except socket.timeout:
         print("Socket timed out")
         print("Maybe check your server's IP?")
@@ -23,10 +27,7 @@ def CheckServerStatus():
     # print(f"Server version : {query.software.version}")
     # print(f"Server MOTD : {status.description}")
     # print(f"Players : {query.players.names}")
-    logging.info(f"Server version : {query.software.version}")
-    logging.info(f"Server MOTD : {status.description}")
-    logging.info(f"Players : {query.players.names}")
-    logging.info(f"==============")
+
 
 if __name__ == '__main__':
     CheckServerStatus()
