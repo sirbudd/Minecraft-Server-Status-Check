c8c0714904770f4f076a5f94397b3dee51478220
Sirbu <dragos.daniel199@gmail.com>
2022-01-07 11:37:14 +0200

    cleanup

--- script.py
+++ script.py
@@ -1,19 +1,17 @@
 import pickle
 from multiprocessing import shared_memory
-import smtplib
 import logging
-import warnings
 from functions import *
 from mcstatus import MinecraftServer
 
-warnings.filterwarnings("ignore")
+
+config = open_cfg()  
 logging.basicConfig(filename='logs.log', level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s') #logging file config
 
-server = MinecraftServer.lookup("157.90.221.188:6055")
-status = server.status()
-print(f"The server has {status.players.online} players and replied in {status.latency} ms")
 
+def CheckServerStatus():
+    server = MinecraftServer.lookup(config['server_ip'])
+    
 
-config = open_cfg()  #open our config file
-# query = server.query()
-# print(f"The server has the following players online: {', '.join(query.players.names)}")
+if __name__ == '__main__':
+    CheckServerStatus()
